9b2649a03540e2e89192c6d1ccd8bb686af5c9c5
Eva Garcia <evagarciaclemente96@gmail.com>
2024-04-28 23:10:45 +0200

    second commit

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-/*
-npm init -y
-npm i express mysql2
-npm i -D nodemon
-
-creas el index.js y creas el git ignore
-te vas a los scripts y los rellenas (el start y el dev)
-haces los require denrto del index.js:
-
-const express = require('express')
-const app = express()
-const PORT = 3000
-app.use(express.json())
-
-const mysql = require('mysql2')
-const db = mysql.createConnection({
-  host: "localhost", 
-  user: "", 
-  password: "", 
-  database: "expressDB // esto simula el USE database del workbench, es decir, lo que haces para seleccionar la db una vez la has creado. esto no se añade hasta que no has creado la base de datos!!!!!!!!!
-}")
-db.connect()
-
-app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`))  ------ el listen va siempre al final
-
-*/
-
-
-/**EXPLICAR EN EL README LO DEL database.example.js */
-
-/** index.js + routes */
-
-
 const express = require("express");
 const app = express();
 const PORT = 3000;
